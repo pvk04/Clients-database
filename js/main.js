2d6669a75f783374f26d4a7bb3840550794bbff7
Alex <97796170+pvk04@users.noreply.github.com>
2022-05-30 16:11:03 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const clients = [
+    {
+        id: "",
+        fio: "",
+        createTime: new Date().now(),
+        lastChange: createTime,
+        contacts: [
+            {vk: "Алексей Громов"},
+            {phoneNum: "124457589"}
+        ]
+    }
+]
+
+function renderClients(){
+    for (client of clients){
+        
+    }
+}
